7bd180e8f17e314c52bcf2809628ffc0b5d6a4fe
Gustavo Ribeiro <gugahribeiro@hotmail.com>
2021-04-25 22:40:25 -0300

    chore: type api customers response

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,38 @@
-import React, { useState, useCallback } from 'react';
-
-import { Container } from './styles';
+import React, { useState, useEffect, useCallback } from 'react';
+import api from '../../services/api';
+import { useAuth } from '../../hooks/auth';
 
 import Header from '../../components/Header';
 import NewCustomerModal from '../../components/NewCustomerModal';
+import CustomersTable from '../../components/CustomersTable';
+import Pagination from '../../components/Pagination';
 
-const Dashboard:React.FC = () => {
+import { Container } from './styles';
+
+interface Customer {
+  id: string;
+  name: string;
+  age: number;
+  marital_status: string;
+  cpf: string;
+  city: string;
+  state: string;
+}
+
+const Dashboard: React.FC = () => {
+  const [customers, setCustomers] = useState<Customer[]>([]);
+  const [page, setPage] = useState(1);
+
+  const { user } = useAuth();
+
+  useEffect(() => {
+    api.get('/customers', {
+      params: {
+        page,
+      },
+    }).then((response) => setCustomers(response.data));
+  }, [page]);
+
   const [isNewCustomerModalOpen, setIsNewCustomerModalOpen] = useState(false);
 
   const handleOpenNewCustomerModal = useCallback(() => {
@@ -15,6 +42,15 @@ const Dashboard:React.FC = () => {
   const handleCloseNewCustomerModal = useCallback(() => {
     setIsNewCustomerModalOpen(false);
   }, []);
+
+  const handlePreviousPage = useCallback(() => {
+    setPage(page - 1);
+  }, [page]);
+
+  const handleNextPage = useCallback(() => {
+    setPage(page + 1);
+  }, [page]);
+
   return (
     <Container>
       <Header onOpenNewCustomerModal={handleOpenNewCustomerModal} />
@@ -22,7 +58,14 @@ const Dashboard:React.FC = () => {
         isOpen={isNewCustomerModalOpen}
         onRequestClose={handleCloseNewCustomerModal}
       />
-
+      <CustomersTable
+        customers={customers}
+      />
+      <Pagination
+        handleNextPage={handleNextPage}
+        handlePreviousPage={handlePreviousPage}
+        page={page}
+      />
     </Container>
   );
 };
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -4,4 +4,10 @@ export const Container = styled.main`
     max-width:1120px;
     margin: 0 auto;
     padding: 2.5rem 1rem;
+
+    section{
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
 `;
