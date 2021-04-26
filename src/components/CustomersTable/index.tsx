2aa369163a72c51dece19ee70c474cb19e3ca014
Gustavo Ribeiro <gugahribeiro@hotmail.com>
2021-04-25 22:35:40 -0300

    chore: add dashboard customers table

--- src/components/CustomersTable/index.tsx
+++ src/components/CustomersTable/index.tsx
@@ -1,33 +1,66 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
+import { FiEdit } from 'react-icons/fi';
+
+import { Link } from 'react-router-dom';
+
 import { Container } from './styles';
 
-const CustomersTable: React.FC = () => (
-  <Container>
-    <table>
-      <thead>
-        <tr>
-          <th>Título</th>
-          <th>Valor</th>
-          <th>Categoria</th>
-          <th>Data</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Desevolvimento de website</td>
-          <td>$12.000.00</td>
-          <td>Freelance</td>
-          <td>20/03/2021</td>
-        </tr>
-        <tr>
-          <td>Desevolvimento de website</td>
-          <td>$12.000.00</td>
-          <td>Freelance</td>
-          <td>20/03/2021</td>
-        </tr>
-      </tbody>
-    </table>
-  </Container>
-);
+interface Customer {
+  id: string;
+  name: string;
+  age: number;
+  marital_status: string;
+  cpf: string;
+  city: string;
+  state: string;
+}
+
+interface CustomersTableProps {
+  customers: Customer[]
+}
+
+const CustomersTable: React.FC<CustomersTableProps> = ({ customers }) => {
+  const [isEditCustomerModalOpen, setIsEditCustomerModalOpen] = useState(false);
+
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Marital Status</th>
+            <th>CPF</th>
+            <th>City</th>
+            <th>State</th>
+          </tr>
+        </thead>
+        <tbody>
+          {customers.map((customer) => (
+            <tr key={customer.id}>
+              <td>{customer.name}</td>
+              <td>{customer.age}</td>
+              <td>{customer.marital_status}</td>
+              <td>{customer.cpf}</td>
+              <td>{customer.city}</td>
+              <td>{customer.state}</td>
+              <td>
+                <Link to={{
+                  pathname: `/customers/${customer.id}`,
+                  state: { customer_id: customer.id },
+                }}
+                >
+                  <FiEdit color="#00B7F8" />
+                </Link>
+              </td>
+            </tr>
+          ))}
+
+        </tbody>
+      </table>
+
+    </Container>
+  );
+};
 
 export default CustomersTable;
--- src/components/CustomersTable/styles.ts
+++ src/components/CustomersTable/styles.ts
@@ -1,29 +1,38 @@
 import styled from 'styled-components';
+import { colors } from '../../styles/global';
 
 export const Container = styled.div`
         margin-top: 4rem;
-
+        border: 1px solid red;
+        padding: 4.2rem 4.8rem;
+        background-color: ${colors.container};
+        border-radius: 10px;
+        border: 1px solid ${colors.stroke};
     table {
         width: 100%;
         border-spacing: 0 0.5rem;
 
         th{
-            color: #969CB2;
-            padding: 1rem 2rem;
-            line-height: 1.5rem;
-            text-align: left;
+          color: #455280;
+          padding: 1rem 2rem;
+          line-height: 1.5rem;
+          text-align: left;
+          border-radius: 10px;
+          font-weight: 500;
+          font-size: 1.2rem;
         }
 
         td{
-            padding: 1rem 2rem;
-            border:0;
-            background-color: #ddd;
-            color: #000;
-            border-radius: calc(1rem / 4);
+          padding: 1rem 2rem;
+          font-size: 1.1rem;
+          border:0;
+          background-color: #fff;
+          color: #969CB2;
+          border-radius: calc(1rem / 4);
 
-            &:first-child{
-                color: var(--text-title);
-            }
+          &:first-child{
+              color: #363F5F;
+          }
         }
     }
 `;
