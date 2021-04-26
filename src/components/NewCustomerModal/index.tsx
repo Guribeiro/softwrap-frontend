aca864397442147cdf7ff51e957ea58c837035ff
Gustavo Ribeiro <gugahribeiro@hotmail.com>
2021-04-25 22:44:14 -0300

    chore: finish submit form config

--- src/components/NewCustomerModal/index.tsx
+++ src/components/NewCustomerModal/index.tsx
@@ -1,12 +1,16 @@
-import React, { useState, useCallback, FormEvent } from 'react';
+import React, { useCallback, useRef } from 'react';
 import Modal from 'react-modal';
+
+import * as Yup from 'yup';
+
+import { FormHandles } from '@unform/core';
+import { useHistory } from 'react-router-dom';
+import getValidationErros from '../../utils/getValidationError';
 
 import { Form } from './styles';
 import closeIcon from '../../assets/close.svg';
-// import incomeIcon from '../../assets/income.svg';
-// import outcomeIcon from '../../assets/outcome.svg';
 
-// import { api } from '../../service/api';
+import api from '../../services/api';
 
 import Input from '../Input';
 import Row from '../Row';
@@ -14,6 +18,7 @@ import Column from '../Column';
 import MaskedInput from '../MaskedInput';
 
 import Select from '../Select';
+import { useToast } from '../../hooks/toast';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -29,10 +34,66 @@ interface NewCustomerFormData {
   state: string;
 }
 
-const NewTransactionModal: React.FC<NewTransactionModalProps> = ({ isOpen, onRequestClose }) => {
-  const handleSubmit = useCallback(async (data: NewCustomerFormData) => {
-    console.log(data);
-  }, []);
+const NewCustomerModal: React.FC<NewTransactionModalProps> = ({ isOpen, onRequestClose }) => {
+  const formRef = useRef<FormHandles>(null);
+  const { addToast } = useToast();
+
+  const history = useHistory();
+
+  const handleSubmit = useCallback(async ({
+    name, age, marital_status, cpf, city, state,
+  }:NewCustomerFormData) => {
+    try {
+      formRef.current?.setErrors({});
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        age: Yup.number()
+          .required('Idade obrigatória')
+          .integer('Digite uma idade válida'),
+        marital_status: Yup.string().required(),
+        cpf: Yup.string().min(11).required(),
+        city: Yup.string().required(),
+        state: Yup.string().required(),
+      });
+
+      await schema.validate({
+        name,
+        age,
+        marital_status,
+        cpf,
+        city,
+        state,
+      }, {
+        abortEarly: false,
+      });
+
+      await api.post('/customers', {
+        name,
+        age,
+        marital_status,
+        cpf,
+        city,
+        state,
+      });
+
+      addToast({
+        type: 'success',
+        title: 'New customer has been saved successfully',
+      });
+
+      history.go(0);
+    } catch (err) {
+      if (err instanceof Yup.ValidationError) {
+        const erros = getValidationErros(err);
+        formRef.current?.setErrors(erros);
+      }
+      addToast({
+        type: 'error',
+        title: 'Something went wrong',
+        description: err.message,
+      });
+    }
+  }, [addToast, history]);
 
   return (
     <Modal
@@ -49,7 +110,7 @@ const NewTransactionModal: React.FC<NewTransactionModalProps> = ({ isOpen, onReq
       >
         <img src={closeIcon} alt="Fechar modal" />
       </button>
-      <Form onSubmit={handleSubmit}>
+      <Form ref={formRef} onSubmit={handleSubmit}>
         <h1>Register Customer</h1>
         <Row>
           <Column>
@@ -68,7 +129,7 @@ const NewTransactionModal: React.FC<NewTransactionModalProps> = ({ isOpen, onReq
             />
           </Column>
           <Column>
-            <MaskedInput name="cpf" mask="999.999.999-99" placeholder="___.___.___-__" />
+            <MaskedInput name="cpf" mask="999.999.999-99" placeholder="CPF" />
             <Input name="city" placeholder="City" />
             <Input name="state" placeholder="State" />
           </Column>
@@ -80,4 +141,4 @@ const NewTransactionModal: React.FC<NewTransactionModalProps> = ({ isOpen, onReq
   );
 };
 
-export default NewTransactionModal;
+export default NewCustomerModal;
